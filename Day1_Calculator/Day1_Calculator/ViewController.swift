cf3acb6d97dbfb7081787847cdf3e9e44e24b29b
rhishikeshmore <rhishikeshmore234@gmail.com>
2023-12-07 14:32:29 +0530

    added day2

---
+++ Day1_Calculator/Day1_Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Day1_Calculator
+//
+//  Created by Apple on 03/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
